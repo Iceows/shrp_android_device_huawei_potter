62308c7542290fe2d53a2c3847eee4aa623b75d8
Raphael Mounier <mounierr07@gmail.com>
2026-01-25 17:53:02 +0100

    patches: add f2fs patch - remove nat bits flags

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,3 +18,16 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
 ccache -M 15G
+
+# Apply necessary F2FS-tools patch
+cd $(realpath .)/external/f2fs-tools/
+if git apply -p1 $OLDPWD/device/huawei/charlotte/prebuilts/0001-f2fs-tools-remove-CP_NAT_BITS_FLAG.patch; then
+    echo "Patch applied to F2FS-tools"
+    cd $OLDPWD
+elif git apply -p1 --reverse --check $OLDPWD/device/huawei/charlotte/prebuilts/0001-f2fs-tools-remove-CP_NAT_BITS_FLAG.patch; then
+    echo "F2FS-tools patch has already been applied, skipping..."
+    cd $OLDPWD
+else
+    echo "F2FS-tools patch could not be applied automatically, it must be done manually. Aborting..."
+    cd $OLDPWD
+fi
