c706a27d61ee0293f578ae047f5e22bedb4a0866
Raphael Mounier <mounierr07@gmail.com>
2026-03-08 13:28:05 +0100

    device: removes libraries contained by TW_INCLUDE_CRYPTO - part1

--- device.mk
+++ device.mk
@@ -32,80 +32,36 @@ PRODUCT_PACKAGES += \
     
 # Keymaster + libteec
 TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.keymaster@3.0 \
-    android.hardware.keymaster@4.0 \
-    android.hardware.keymaster@4.1 \
-    android.hardware.gatekeeper@1.0 \
-    android.hardware.confirmationui@1.0 \
     libkeymaster3device \
-    libkeymaster4support \
-    libkeymaster4_1support \
     libkeymaster_portable \
     libkeymaster_messages \
-    libprotobuf-cpp-lite \
     libpuresoftkeymasterdevice \
     libsoft_attestation_cert \
     libcppbor_external \
     libcppbor \
-    libkeystore-engine-wifi-hidl \
     libcppcose_rkp \
     libhwbinder \
-    libkeystore-wifi-hidl \
+    android.hardware.keymaster@3.0 \
     android.hidl.memory@1.0 \
     android.hidl.memory.token@1.0 \
-    libgatekeeper \
-    libkeystore_binder \
-    libsoftkeymasterdevice \
-    libsoftkeymaster \
-    libkeystore_aidl \
-    libkeystore_parcelables \
-    libutilscallstack \
-    libdexfile \
-    libservices \
-    libkeystore-attestation-application-id \
-    libtwrpfscrypt \
-    libstatslog \
-    libstatssocket \
     libhidlmemory
     
 # Recovery link
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster3device.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4support.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4_1support.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_portable.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_messages.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libsoftkeymaster.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeystore_aidl.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeystore_parcelables.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libutilscallstack.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libdexfile.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libservices.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeystore-attestation-application-id.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libsoftkeymasterdevice.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libsoft_attestation_cert.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libcppbor_external.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libcppbor.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libcppcose_rkp.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libhwbinder.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@3.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.gatekeeper@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.confirmationui@1.0.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory@1.0.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory.token@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libgatekeeper.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeystore_binder.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libtwrpfscrypt.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libprotobuf-cpp-lite.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libstatslog.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libstatssocket.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libhidlmemory.so
 
-TW_RECOVERY_ADDITIONAL_RELINK_FILES += \
-    $(TARGET_OUT_EXECUTABLES)/keystore
 
 # Blacklist
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
